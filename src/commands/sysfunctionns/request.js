5c799677908bb18f294e3684f9bc75416b1e7d30
therealkeyisme <therealkey@outlook.com>
2020-12-09 23:12:36 -0800

    Added requests cmd

---
+++ src/commands/sysfunctionns/request.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const { jsonReader } = require('../../utils/jsonreader')
+
+module.exports = {
+    run: async(client, message, args, DEVID) => {
+        let returnMessage = "We have informed someone of your request. If you are reporting a bug, please see https://github.com/therealkeyisme/JSbot/issues and start a new issue."
+
+        
+        let devMessage = "Req/Iss:  ```" + args + "```";
+        let devUser = client.users.fetch(DEVID)
+            .then(value => {
+                value.send(devMessage)
+            })
+        
+
+        jsonReader('src/data/botdata.json', (err, data) => {
+            if (err) {
+                console.log(err);
+            } else {
+                data.requests.push(args);
+
+                fs.writeFile('src/data/botdata.json', JSON.stringify(data, null, 2), err => {
+                    if (err) {
+                        console.log(err);
+                    }
+                    else {
+                        message.channel.send(returnMessage);
+                    }
+                })
+            }
+        })  
+        // !
+    },
+    aliases: ['report', 'issue']
+}
