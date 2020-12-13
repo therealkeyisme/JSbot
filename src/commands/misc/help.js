9ccdba788d94f6c70446b560236d03051d635efd
therealkeyisme <therealkey@outlook.com>
2020-12-13 11:37:07 -0800

    Added more to the help list

--- src/commands/misc/help.js
+++ src/commands/misc/help.js
@@ -4,7 +4,9 @@ module.exports = {
         let embedMessage = "-`?roll` rolls a dice\n-`?request` reports a bug\n-`?shopping` maintains and stores a " +
             "shopping list\n-`?help` displays this message\n-`?ban` bans people\n-`?kick` kicks people\n-`?request` " +
             "sends an issue to the bot developer\n-`?addrole` adds a role to a player\n-`?delrole` removes a role from " +
-            "a player\n-`?mute` mutes a player\n-`?unmute` unmutes a player"
+            "a player\n-`?mute` mutes a player\n-`?unmute` unmutes a player\n-`?play` plays a song in the voice channel" +
+            "\n`?skip` skips the current song\n-`?np` shows the current playing song\n-`?pause` pauses the song thats " +
+            "playing"
         let embed = {
             title : "Do you need help with any of these commands?",
             color : '#63d6ff',
