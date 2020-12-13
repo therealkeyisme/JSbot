2588482605a944375acfcfd0b7013c77f789cb0a
therealkeyisme <therealkey@outlook.com>
2020-12-13 11:37:18 -0800

    Adjusted volume settings

--- src/commands/music/volume.js
+++ src/commands/music/volume.js
@@ -5,9 +5,9 @@ module.exports = {
         const serverQueue = message.client.queue.get(message.guild.id);
         if (!serverQueue) return message.channel.send('There is nothing playing.');
         if (!args[0]) return message.channel.send(`The current volume is: **${serverQueue.volume}**`);
-        serverQueue.volume = args[0]; // eslint-disable-line
-        serverQueue.connection.dispatcher.setVolumeLogarithmic(args[0] / 5);
-        return message.channel.send(`I set the volume to: **${args[0]}**`);
+        serverQueue.volume = args; // eslint-disable-line
+        serverQueue.connection.dispatcher.setVolumeLogarithmic(args / 5);
+        return message.channel.send(`I set the volume to: **${args}**`);
     },
     aliases: [],
     description: "Changes the volume for the music bot"
