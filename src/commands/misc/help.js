cce41bb4be7e558e88842f52314947bf93e30790
therealkeyisme <therealkey2001@gmail.com>
2020-12-12 22:59:29 -0800

    Create help.js

---
+++ src/commands/misc/help.js
@@ -0,0 +1,18 @@
+const discord = require('discord.js');
+
+module.exports = {
+    run: async(client, message, args) => {
+        let embedMessage = "-`?roll` rolls a dice\n-`?request` reports a bug\n-`?shopping` maintains and stores a " +
+            "shopping list\n-`?help` displays this message\n-`?ban` bans people\n-`?kick` kicks people\n-`?request` " +
+            "sends an issue to the bot developer\n-`?addrole` adds a role to a player\n-`?delrole` removes a role from " +
+            "a player\n-`?mute` mutes a player\n-`?unmute` unmutes a player"
+        let embed = {
+            title : "Do you need help with any of these commands?",
+            color : '#63d6ff',
+            description: embedMessage
+        }
+        await message.channel.send( {embed});
+    },
+    aliases: ['?'],
+    description: ""
+}
