55f85e5b5d26213c54e71ab84bce4a672962b4d3
therealkeyisme <therealkey2001@gmail.com>
2020-12-15 16:48:00 -0800

    Prevented EarRape

--- src/commands/music/volume.js
+++ src/commands/music/volume.js
@@ -5,9 +5,14 @@ module.exports = {
         const serverQueue = message.client.queue.get(message.guild.id);
         if (!serverQueue) return message.channel.send('There is nothing playing.');
         if (!args[0]) return message.channel.send(`The current volume is: **${serverQueue.volume}**`);
-        serverQueue.volume = args; // eslint-disable-line
-        serverQueue.connection.dispatcher.setVolumeLogarithmic(args / 5);
-        return message.channel.send(`I set the volume to: **${args}**`);
+        if(args > 1) {
+            return message.channel.send("You cannot set the volume that high. you will die")
+        }
+        else {
+            serverQueue.volume = args; // eslint-disable-line
+            serverQueue.connection.dispatcher.setVolumeLogarithmic(args);
+            return message.channel.send(`I set the volume to: **${args}**`);
+        }
     },
     aliases: [],
     description: "Changes the volume for the music bot"
