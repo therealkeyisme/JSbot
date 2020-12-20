8447c74acbd37bcc898ee4a538e1cd9b29e10570
therealkeyisme <therealkey2001@gmail.com>
2020-12-20 09:58:09 -0800

    Update setup.py

--- config/setup.py
+++ config/setup.py
@@ -2,7 +2,8 @@ import json
 import csv
 import os
 import os.path
-DATA_LOCATION = "src\\data\\botdata.json"
+from sys import platform
+
 
 # TODO: Make sure that the requests work
 
@@ -29,20 +30,60 @@ PREFIX = input("What one character call do you want for your bot prefix?\n")
 BOT_OWNER = input("What is your personal discord ID?\n")
 BABY_BOT = input("Were you using the baby_bot discord bot before? Y or N\n")
 
-if not os.path.exists(DATA_LOCATION):
-    with open(DATA_LOCATION, 'w') as json_file:
-        toWrite = datajsonformat("testserver", ['testlist'])
-        json.dump(toWrite, json_file)
-
-# TODO Change to YN
+print(platform)
 
 
-if BABY_BOT.lower() == "y":
-    retrieveData = input("Would you like us to retrieve the data you had as a part of baby-bot? Y or N\n")
+if platform == 'linux' or platform == 'linux2':
+    DATA_LOCATION = "src/data/botdata.json"
+
+    if not os.path.exists(DATA_LOCATION):
+        with open(DATA_LOCATION, 'w') as json_file:
+            toWrite = datajsonformat("testserver", ['testlist'])
+            json.dump(toWrite, json_file)
+
+    if BABY_BOT.lower() == "y":
+        retrieveData = input("Would you like us to retrieve the data you had as a part of baby-bot? Y or N\n")
     if retrieveData.lower() == 'y':
         dataLocation = input("Please enter the path to the root of the baby-bot folder\n")
 
-        currentDirectory = os.getcwd();
+        currentDirectory = os.getcwd()
+        
+        with open(DATA_LOCATION, 'r') as json_file:
+            readData = json.load(json_file)
+
+        for root,dirs,files in os.walk(dataLocation):
+            for file in files:
+                if file.endswith(".csv") and len(file) == 30:
+                    # TODO: Make it so that for each file it analyzes the current json file and writes the server ID etc to the datajsonformat using the function
+                    serverID = file[8:-4]
+                    print(serverID)
+                    
+
+                    with open(dataLocation + "//" + file) as csv_file:
+                        reader = csv.DictReader(csv_file)
+                        shoppinglist = []
+                        for row in reader:
+                            shoppinglist.append(row['Shopping List'])
+
+
+                    createdjson = jsonalreadycreated(serverID, shoppinglist)
+                    readData["server"].append(createdjson)
+
+
+elif platform == 'win32':
+    DATA_LOCATION = "src\\data\\botdata.json"
+
+    if not os.path.exists(DATA_LOCATION):
+        with open(DATA_LOCATION, 'w') as json_file:
+            toWrite = datajsonformat("testserver", ['testlist'])
+            json.dump(toWrite, json_file)
+
+    if BABY_BOT.lower() == "y":
+        retrieveData = input("Would you like us to retrieve the data you had as a part of baby-bot? Y or N\n")
+    if retrieveData.lower() == 'y':
+        dataLocation = input("Please enter the path to the root of the baby-bot folder\n")
+
+        currentDirectory = os.getcwd()
         
         with open(DATA_LOCATION, 'r') as json_file:
             readData = json.load(json_file)
@@ -64,10 +105,7 @@ if BABY_BOT.lower() == "y":
 
                     createdjson = jsonalreadycreated(serverID, shoppinglist)
                     readData["server"].append(createdjson)
-                    
-
-
-
+                 
 with open(DATA_LOCATION, 'w') as json_file:
     json.dump(readData, json_file, indent=4)                    
 
