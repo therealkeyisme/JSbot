75c0310134939d7e752c59c52032ea765387e0f3
therealkeyisme <therealkey@outlook.com>
2020-12-21 17:22:49 -0800

    Changed Database to be more modular

--- src/database/database.js
+++ src/database/database.js
@@ -1,2 +1,5 @@
 const mongoose = require('mongoose');
-module.exports = mongoose.connect('mongodb://localhost:27017/rolereaction', {useNewUrlParser: true, useUnifiedTopology: true});
+module.exports = mongoose.connect(
+    'mongodb://localhost:27017/BabyBotDB',
+    { useNewUrlParser: true, useUnifiedTopology: true },
+);
