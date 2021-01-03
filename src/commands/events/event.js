cc08619208eaf0e6882a8d228f1a3e67310b1242
therealkeyisme <therealkey@outlook.com>
2021-01-02 18:33:53 -0800

    Connected event.js to DB

--- src/commands/events/event.js
+++ src/commands/events/event.js
@@ -3,44 +3,100 @@
 // TODO: Finish the direct messages here
 // TODO: impliment the title, date, and description messages
 // TODO: Write the recurring function checkForEvents
-// TODO: Figure out how to interpret time
 // TODO: celebrate because we finished?
+// TODO: Fix database problems
 const EventModel = require('../../database/models/eventSchema');
-const { checkDbEvents, informationFromUser, findDate } = require('../../utils/eventfn')
+const { informationFromUser, findDate } = require('../../utils/eventfn')
+const Discord = require('discord.js')
 
+let dbAnalysis = async(eventDocument, GUILDID, title, description, eventEmbed, event) => {
+    if(!eventDocument) {
+        let dbModel = new EventModel({
+            events: [
+                {
+                    guildid: GUILDID,
+                    title: title,
+                    date: event,
+                    description: description,
+                    messageId: eventEmbed.id
+                }
+            ]
+        })
+        await dbModel.save()
+    }
+    else {
+        let eventList = eventDocument.events;
+        eventList.push({
+            title: title,
+            date: event,
+            description: description,
+            messageid: eventEmbed.id
+        })
+        console.log(eventList)
+        await dbUpdate(eventDocument, eventList)
+    }
+}
+
+let dbUpdate = async(eventDocument, eventList) => {
+    let newEventList = {events: eventList}
+    await eventDocument.updateOne(newEventList)
+}
 
 module.exports = {
-    run: async (client, message, args) => {
+    run: async (client, message) => {
         if(!message.guild) {
             return
         }
+        const regExpObj ={
+            allWeekDays: /\b((mon|tues|wed(nes)?|thur(s)?|fri|sat(ur)?|sun)(day)?)\b/,
+            monday: /\b((mon)(day)?)\b/,
+            tuesday: /\b((tues)(day)?)\b/,
+            wednesday: /\b((wed(nes)?)(day)?)\b/,
+            thursday: /\b((thur(s)?)(day)?)\b/,
+            friday: /\b((fri)(day)?)\b/,
+            saturday: /\b((sat(ur)?)(day)?)\b/,
+            sunday: /\b((sun)(day)?)\b/,
+            amOrPm: /\b([0]?[0-9]|1[0-2]):?([0-5]?\d?)([ap])m\b/,
+            am: /\b([0]?[0-9]|1[0-2]):?([0-5]?\d?)am\b/,
+            pm: /\b([0]?[0-9]|1[0-2]):?([0-5]?\d?)pm\b/,
+            date: /\b(\d{4})([\/\-])(0?[1-9]|1[012])\2(0?[1-9]|[12]\d|3[01])\b/,
+            time24hr: /\b([01]?[0-9]|2[0-3]):([0-5]\d)\b/,
+            tomorrow: /\b(tomorrow)\b/,
+            now: /\b(now)\b/
+        }
+
         const GUILDID = message.guild.id;
         // TODO: Make this go away once the dates are done
-        const filter = (m) => m.content.startsWith("")
+        const defaultFilter = (m) => m.content.startsWith("")
+        const timeDateFiler = (m) => {
+            let { allWeekDays, amOrPm, date, time24hr, tomorrow, now } = regExpObj;
+            if (allWeekDays.test(m)) return true
+            if (amOrPm.test(m)) return true
+            if (date) return true
+            if (time24hr) return true
+            if (tomorrow) return true
+            if (now) return true
+        }
         let author = message.author
         try {
-            let dmChannel = await author.createDM(true)
-            let eventDocument = await EventModel.findOne({
-                guildId: GUILDID
-            });
-            if(!eventDocument) {
-
-            };
+            let dmChannel = await author.createDM()
+            let eventDocument = await EventModel.findOne();
             let embed = {
                 title: "Enter the event title",
                 description: "Please keep it short",
                 color: '#63d6ff'
             };
 
-            // let title = await informationFromUser(dmChannel, embed);
+            let title = await informationFromUser(dmChannel, embed, defaultFilter);
 
-            // embed = {
-            //     title: "Enter the event description",
-            //     description: "Type none for no description",
-            // };
+            embed = {
+                title: "Enter the event description",
+                description: "Type none for no description",
+            };
             
-            // let description = await informationFromUser(dmChannel, embed);
+            let description = await informationFromUser(dmChannel, embed, defaultFilter);
 
+            // TODO: Add time zone support
             // embed = {
             //     title: "Enter timezone number",
             //     description: "If your time zone is missing use `?request.`"
@@ -53,86 +109,26 @@ module.exports = {
                 description: "```Friday at 9pm\n Tomorrow at 18:00\n Now\n In 1 hour\n YYYY-MM-DD 7:00 PM```"
             }
 
-            let startTime = (await informationFromUser(dmChannel, embed, filter)).toLowerCase();
+            let startTime = (await informationFromUser(dmChannel, embed, timeDateFiler)).toLowerCase();
 
-            const regExpObj ={
-                allWeekDays: /\b((mon|tues|wed(nes)?|thur(s)?|fri|sat(ur)?|sun)(day)?)\b/,
-                monday: /\b((mon)(day)?)\b/,
-                tuesday: /\b((tues)(day)?)\b/,
-                wednesday: /\b((wed(nes)?)(day)?)\b/,
-                thursday: /\b((thur(s)?)(day)?)\b/,
-                friday: /\b((fri)(day)?)\b/,
-                saturday: /\b((sat(ur)?)(day)?)\b/,
-                sunday: /\b((sun)(day)?)\b/,
-                amOrPm: /\b([0]?[0-9]|1[0-2]):?([0-5]?\d?)([ap])m\b/,
-                am: /\b([0]?[0-9]|1[0-2]):?([0-5]?\d?)am\b/,
-                pm: /\b([0]?[0-9]|1[0-2]):?([0-5]?\d?)pm\b/,
-                date: /\b(\d{4})([\/\-])(0?[1-9]|1[012])\2(0?[1-9]|[12]\d|3[01])\b/,
-                time24hr: /\b([01]?[0-9]|2[0-3]):([0-5]\d)\b/,
-                tomorrow: /\b(tomorrow)\b/,
-                now: /\b(now)\b/
-            }
-            const weekdays = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
-            const today = new Date(Date.now())
-            const todayDay = today.getDay() + 1 // Day number
-            const todayDate = today.getDate() // Date number
-            const todayMonth = today.getMonth()
-            const todayYear = today.getFullYear()
-            const todayHours = today.getHours()
-            const todayMinutes = today.getMinutes()
-            let event = today;
-            let eventDay = todayDay;
-            let eventDate = todayDate;
-            let eventMonth = todayMonth;
-            let eventYear = todayYear;
-            let eventHours = todayHours;
-            let eventMinutes = todayMinutes;
-            let weekDayNumber;
-            // Testing to see if a day of the week is chosen
-            if (regExpObj.allWeekDays.test(startTime)) {
-                eventDay = (regExpObj.allWeekDays.exec(startTime))[1]
-                for (let i=0; i < weekdays.length; i++) {
-                    if (weekdays[i] === eventDay)
-                        weekDayNumber = i + 1
-                }
-                if (weekDayNumber < todayDay || weekDayNumber === todayDay) {
-                    weekDayNumber += 7;
-                }
-                eventDate = (weekDayNumber - todayDay) + todayDate;
-            }
-            else if (regExpObj.date.test(startTime)) {
-                eventYear = regExpObj.date.exec(startTime)[1]
-                eventMonth = parseInt(regExpObj.date.exec(startTime)[3]) - 1
-                eventDate = regExpObj.date.exec(startTime)[4]
-            }
-            else if(regExpObj.tomorrow.test(startTime)) {
-                eventDate = todayDate + 1;
-            }
-            // Testing to see if its am or pm... then if it is then which one
-            if (regExpObj.amOrPm.test(startTime)) {
-                eventHours = parseInt(regExpObj.amOrPm.exec(startTime)[1])
-                eventMinutes = parseInt(regExpObj.amOrPm.exec(startTime)[2])
-                if (regExpObj.pm.test(startTime)) {
-                    eventHours += 12;
-                }
-                if (!eventMinutes) {
-                    eventMinutes = 0
-                }
-            }
-            else if (regExpObj.time24hr.test(startTime)) {
-                eventHours = regExpObj.time24hr.exec(startTime)[1]
-                eventMinutes = regExpObj.time24hr.exec(startTime)[2]
-            }
-
-            event.setFullYear(eventYear);
-            event.setMonth(eventMonth);
-            event.setDate(eventDate);
-            event.setHours(eventHours);
-            event.setMinutes(eventMinutes)
-            event.setSeconds(0);
-            event.setMilliseconds(0);
+            let event = findDate(startTime, regExpObj)
 
             console.log(`${event.getFullYear()}-${event.getMonth()}-${event.getDate()} ${event.getHours()}:${event.getMinutes()}`)
+            
+            let eventMinutes = event.getMinutes()
+            let eventMonth = event.getMonth() + 1
+
+            if (eventMinutes === 0) {
+                eventMinutes = "00"
+            }
+            
+            
+            let returnEmbed = new Discord.MessageEmbed()
+                .setTitle(title)
+                .setDescription(description)
+                .setFooter(`${event.getFullYear()}-${eventMonth}-${event.getDate()} ${event.getHours()}:${eventMinutes}`)
+            let eventEmbed = await message.channel.send(returnEmbed)
+            await dbAnalysis(eventDocument, GUILDID, title, description, eventEmbed, event)
         }
         catch(err) {
             console.log(err)
--- src/database/models/eventSchema.js
+++ src/database/models/eventSchema.js
@@ -1,14 +1,13 @@
 const mongoose = require('mongoose');
 const EventSchema = new mongoose.Schema({
-    guildId: { type: String, required: true },
-    events: [
-        {
-            title: String,
-            date: Date,
-            description: String,
-            messageId: String
-        }
-    ]
+    events: [{
+        guildid: String,
+        title: String,
+        date: Date,
+        description: String,
+        messageid: String,
+        attendees: [String]
+    }]
 })
 
 const EventModel = (module.exports = mongoose.model(
