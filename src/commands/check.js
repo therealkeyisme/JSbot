61a441b527b7918457184985ac94f74e5348eebd
therealkeyisme <therealkey@outlook.com>
2021-01-05 23:31:20 -0800

    Deleted check

--- src/commands/check.js
+++
@@ -1,9 +0,0 @@
-const EventModel = require('../../database/models/eventSchema')
-const Discord = require('discord.js')
-module.exports = {
-    run: async (client, message, args) => {
-        
-    },
-    aliases: [],
-    description: ""
-}
