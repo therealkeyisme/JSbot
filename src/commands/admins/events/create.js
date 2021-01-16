822e0477405605c79dadd0c9dc888277d8668c56
therealkeyisme <therealkey@outlook.com>
2021-01-15 21:38:58 -0800

    Moved Create.js

--- src/commands/admins/events/create.js
+++ src/commands/admins/events/create.js
@@ -4,21 +4,30 @@ const PrefModel = require('../../../database/models/prefSchema');
 module.exports = {
     run: async (client, message, args) => {
         const GUILDID = message.guild.id;
-        let prefDocument = await PrefModel.findOne({ guildid: GUILDID });
-        let newChannel = await message.guild.channels.create('BabyBot Events', {
-            type: 'text',
-            topic: 'events',
-        });
-        console.log();
-        if (prefDocument) {
-            let newEventChannel = { eventChannel: newChannel.id };
-            await prefDocument.updateOne(newEventChannel);
+        if (!message.member.hasPermission(['ADMINISTRATOR'])) {
+            message.channel.send(
+                "You don't have permission to use this command",
+            );
         } else {
-            let dbPrefModel = new PrefModel({
-                guildid: GUILDID,
-                eventChannel: newChannel.id,
-            });
-            await dbPrefModel.save();
+            let prefDocument = await PrefModel.findOne({ guildid: GUILDID });
+            let newChannel = await message.guild.channels.create(
+                'BabyBot Events',
+                {
+                    type: 'text',
+                    topic: 'events',
+                },
+            );
+            console.log();
+            if (prefDocument) {
+                let newEventChannel = { eventChannel: newChannel.id };
+                await prefDocument.updateOne(newEventChannel);
+            } else {
+                let dbPrefModel = new PrefModel({
+                    guildid: GUILDID,
+                    eventChannel: newChannel.id,
+                });
+                await dbPrefModel.save();
+            }
         }
     },
     aliases: ['createchannel'],
